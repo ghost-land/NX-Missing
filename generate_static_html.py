25fdc6ad457f62849fe9a4be42973c864789b763
Ghost0159 <contact.ghost01@gmail.com>
2024-08-19 19:44:29 +0200

    Update generate_static_html.py

--- generate_static_html.py
+++ generate_static_html.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from update_data import download_jsons, config
 import os
+import shutil
 
 app = Flask(__name__)
 
@@ -10,14 +11,24 @@ def render_static_html():
     with app.app_context():
         if not os.path.exists('_site'):
             os.makedirs('_site')
-        
+
+        static_dir = os.path.join('_site', 'static')
+        if not os.path.exists(static_dir):
+            os.makedirs(static_dir)
+            os.makedirs(os.path.join(static_dir, 'css'))
+            os.makedirs(os.path.join(static_dir, 'js'))
+
+        shutil.copy('static/css/style.css', os.path.join(static_dir, 'css/style.css'))
+
+        shutil.copy('static/js/showTables.js', os.path.join(static_dir, 'js/showTables.js'))
+
         rendered = render_template(
             'index.html',
             missing=missing,
             images_size=config.get('images-size', 100),
             len=len
         )
-        with open('_site/index.html', 'w', encoding='utf-8') as f:
+        with open(os.path.join('_site', 'index.html'), 'w', encoding='utf-8') as f:
             f.write(rendered)
 
 if __name__ == "__main__":
